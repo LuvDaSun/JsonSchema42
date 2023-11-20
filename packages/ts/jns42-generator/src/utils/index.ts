51c88237548f3805d52209c9553f84c732af61e9
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-20 10:40:05 +0100

    adds itt

--- packages/ts/jns42-generator/src/utils/index.ts
+++ packages/ts/jns42-generator/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from "./choose.js";
 export * from "./file.js";
+export * from "./iterable-text-template.js";
 export * from "./literal.js";
 export * from "./load.js";
 export * from "./namer.js";
