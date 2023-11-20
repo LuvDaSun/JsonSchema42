534b177f736c2342bfd19b4504b41947d6bcbb84
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-20 15:24:47 +0100

    updaate tests

--- packages/ts/jns42-generator/src/generators/main-test-ts.ts
+++ packages/ts/jns42-generator/src/generators/main-test-ts.ts
@@ -1,93 +1,41 @@
-import ts from "typescript";
 import * as models from "../models/index.js";
-import { itt } from "../utils/index.js";
-import { generateLiteral } from "../utils/literal.js";
-import { CodeGeneratorBase } from "./code-generator-base.js";
+import { itt, toCamel } from "../utils/index.js";
 
 export function* generateMainSpecTsCode(specification: models.Specification) {
   const { names, nodes } = specification;
-  const { factory } = ts;
 
-  const codeGenerator = new MainTestTsCodeGenerator(factory, names, nodes);
-  const code = codeGenerator.getCode();
+  yield itt`
+    import assert from "node:assert/strict";
+    import test from "node:test";
+    import * as main from "./main.js";
+  `;
 
-  return code;
+  for (const nodeId in nodes) {
+    const node = nodes[nodeId];
+    if ((node.metadata.examples ?? []).length === 0) {
+      continue;
+    }
+    const typeName = names[nodeId];
+    const testBody = generateTestBody(specification, nodeId);
+    yield itt`
+      test(${JSON.stringify(typeName)}, () => {
+        ${testBody}
+      })
+    `;
+  }
 }
 
-class MainTestTsCodeGenerator extends CodeGeneratorBase {
-  public *getCode() {
+function* generateTestBody(specification: models.Specification, nodeId: string) {
+  const { nodes, names } = specification;
+  const validatorFunctionName = toCamel("is", names[nodeId]);
+
+  const node = nodes[nodeId];
+  for (const example of node.metadata.examples ?? []) {
     yield itt`
-      import assert from "node:assert/strict";
-      import test from "node:test";
-      import * as main from "./main.js";
+      assert.equal(
+        main.${validatorFunctionName}(${JSON.stringify(example)}),
+        true,
+      )
     `;
-
-    for (const nodeId in this.nodes) {
-      const statements = this.generateTestStatement(nodeId);
-      yield* this.nestedTextFromTs(statements);
-    }
-  }
-
-  protected *generateTestStatement(nodeId: string) {
-    const { factory: f } = this;
-
-    const typeName = this.getTypeName(nodeId);
-
-    const assertStatements = [...this.generateAssertStatements(nodeId)];
-
-    if (assertStatements.length === 0) return;
-
-    yield f.createExpressionStatement(
-      f.createCallExpression(f.createIdentifier("test"), undefined, [
-        f.createStringLiteral(typeName),
-        f.createArrowFunction(
-          undefined,
-          undefined,
-          [
-            f.createParameterDeclaration(
-              undefined,
-              undefined,
-              f.createIdentifier("t"),
-              undefined,
-              undefined,
-              undefined,
-            ),
-          ],
-          undefined,
-          f.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
-          f.createBlock(assertStatements, true),
-        ),
-      ]),
-    );
-  }
-
-  protected *generateAssertStatements(nodeId: string): Iterable<ts.Statement> {
-    const { factory: f } = this;
-    const node = this.nodes[nodeId];
-
-    const typeName = this.getTypeName(nodeId);
-
-    for (const example of node.metadata.examples ?? []) {
-      yield f.createExpressionStatement(
-        f.createCallExpression(
-          f.createPropertyAccessExpression(
-            f.createIdentifier("assert"),
-            f.createIdentifier("equal"),
-          ),
-          undefined,
-          [
-            f.createCallExpression(
-              f.createPropertyAccessExpression(
-                f.createIdentifier("main"),
-                f.createIdentifier(`is${typeName}`),
-              ),
-              undefined,
-              [generateLiteral(f, example)],
-            ),
-            f.createTrue(),
-          ],
-        ),
-      );
-    }
   }
 }
