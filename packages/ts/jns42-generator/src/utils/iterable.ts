1c19a3c99b7dd6fbb7a2e7fdbda2995a066ed29b
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-21 16:28:17 +0100

    prettier output

--- packages/ts/jns42-generator/src/utils/iterable.ts
+++ packages/ts/jns42-generator/src/utils/iterable.ts
@@ -14,3 +14,27 @@ export function* mapIterable<T, R>(iterable: Iterable<T>, mapper: (item: T) => R
     yield mapper(item);
   }
 }
+
+export function* splitIterable(texts: Iterable<string>, separator = /\r?\n/): Iterable<string> {
+  let buffer = "";
+
+  for (const text of texts) {
+    buffer += text;
+
+    yield* flush();
+  }
+
+  if (buffer.length > 0) yield buffer;
+
+  function* flush() {
+    while (true) {
+      const match = separator.exec(buffer);
+      if (!match) break;
+
+      const line = buffer.substring(0, match.index);
+      buffer = buffer.substring(match.index + match[0].length);
+
+      yield line;
+    }
+  }
+}
