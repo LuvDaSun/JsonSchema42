33534581541db2f797f5a0a5e30d181fb53eeffb
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-12-12 11:29:50 +0100

    test wip

--- packages/ts/jns42-optimizer/src/transforms/all-of-one-of.test.ts
+++ packages/ts/jns42-optimizer/src/transforms/all-of-one-of.test.ts
@@ -11,8 +11,12 @@ test("all-of-one-of", () => {
   arena.addItem({ id: null, type: "string" }); // 0
   arena.addItem({ id: null, type: "string" }); // 1
   arena.addItem({ id: null, type: "string" }); // 2
-  arena.addItem({ id: null, type: "oneOf", elements: [1, 2] }); // 3
-  arena.addItem({ id: null, type: "allOf", elements: [0, 3] }); // 4
+  arena.addItem({ id: null, type: "string" }); // 3
+  arena.addItem({ id: null, type: "string" }); // 4
+  arena.addItem({ id: null, type: "string" }); // 5
+  arena.addItem({ id: null, type: "oneOf", elements: [2, 3] }); // 6
+  arena.addItem({ id: null, type: "oneOf", elements: [4, 5] }); // 7
+  arena.addItem({ id: null, type: "allOf", elements: [0, 1, 6, 7] }); // 8
 
   while (arena.applyTransform(...useTransforms) > 0);
 
@@ -22,10 +26,16 @@ test("all-of-one-of", () => {
       { id: null, type: "string" }, // 0
       { id: null, type: "string" }, // 1
       { id: null, type: "string" }, // 2
-      { id: null, type: "oneOf", elements: [1, 2] }, // 3
-      { id: null, type: "oneOf", elements: [5, 6] }, // 4
-      { id: null, type: "allOf", elements: [0, 1] }, // 5
-      { id: null, type: "allOf", elements: [0, 2] }, // 6
+      { id: null, type: "string" }, // 3
+      { id: null, type: "string" }, // 4
+      { id: null, type: "string" }, // 5
+      { id: null, type: "oneOf", elements: [2, 3] }, // 6
+      { id: null, type: "oneOf", elements: [4, 5] }, // 7
+      { id: null, type: "oneOf", elements: [9, 10, 11, 12] }, // 8
+      { id: null, type: "allOf", elements: [0, 1, 2] }, // 9
+      { id: null, type: "allOf", elements: [0, 1, 3] }, // 10
+      { id: null, type: "allOf", elements: [0, 1, 4] }, // 11
+      { id: null, type: "allOf", elements: [0, 1, 5] }, // 12
     ],
   );
   assert(!hasDoubleReference([...arena]));
