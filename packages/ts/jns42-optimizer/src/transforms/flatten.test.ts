136026d52433e9a37c81e182a8be9133f4c74a6f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-12-14 18:52:05 +0100

    strict assertions

--- packages/ts/jns42-optimizer/src/transforms/flatten.test.ts
+++ packages/ts/jns42-optimizer/src/transforms/flatten.test.ts
@@ -1,4 +1,4 @@
-import assert from "node:assert";
+import assert from "node:assert/strict";
 import test from "node:test";
 import { TypeArena } from "../type-arena.js";
 import { deleteUndefined } from "../utils/index.js";
