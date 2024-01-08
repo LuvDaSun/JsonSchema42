21d60da20dcd03b28c026ba20386dcaf0eb32452
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-08 12:55:56 +0100

    empty

--- packages/ts/jns42-optimizer/src/utils/index.ts
+++ packages/ts/jns42-optimizer/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from "./arena.js";
 export * from "./choose.js";
+export * from "./empty.js";
 export * from "./merge.js";
 export * from "./normalize-object.js";
 export * from "./properties.js";
