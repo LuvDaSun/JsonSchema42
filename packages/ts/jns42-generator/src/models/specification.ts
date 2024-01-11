6d677f44109d185a02de6bba7b4bb30a39ae933d
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-11 14:45:05 +0100

    cleanup

--- packages/ts/jns42-generator/src/models/specification.ts
+++ packages/ts/jns42-generator/src/models/specification.ts
@@ -1,9 +1,7 @@
-import * as schemaIntermediate from "schema-intermediate";
 import * as models from "../models/index.js";
 
 export interface Specification {
   names: Record<string, string>;
-  nodes: Record<string, schemaIntermediate.Node>;
   types: Record<string, models.Item | models.Alias>;
   options: {};
 }
