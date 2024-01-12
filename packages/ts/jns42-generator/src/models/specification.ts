8e81bbca404d42e415f708c2aa7bb225fad877cd
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-12 10:49:15 +0100

    add nodes back

--- packages/ts/jns42-generator/src/models/specification.ts
+++ packages/ts/jns42-generator/src/models/specification.ts
@@ -1,7 +1,9 @@
+import * as schemaIntermediate from "schema-intermediate";
 import * as models from "../models/index.js";
 
 export interface Specification {
   names: Record<string, string>;
   types: Record<string, models.Item | models.Alias>;
+  nodes: Record<string, schemaIntermediate.Node>;
   options: {};
 }
