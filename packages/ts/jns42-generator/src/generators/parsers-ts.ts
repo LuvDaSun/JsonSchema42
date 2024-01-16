6091dd10a979c4f1e1ff31716fe4467430aca2da
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-16 10:57:04 +0100

    wip

--- packages/ts/jns42-generator/src/generators/parsers-ts.ts
+++ packages/ts/jns42-generator/src/generators/parsers-ts.ts
@@ -37,7 +37,7 @@ export function* generateParsersTsCode(specification: models.Specification) {
     }
 
     const functionName = toCamel("parse", names[nodeId]);
-    const definition = generateParserDefinition(specification, typeKey);
+    const definition = generateParserDefinition(specification, typeKey, "value");
 
     yield itt`
       ${generateJsDocComments(item)}
@@ -47,7 +47,7 @@ export function* generateParsersTsCode(specification: models.Specification) {
           ...options,
         };
 
-        ${definition}
+        return (${definition});
       }
     `;
   }
@@ -61,194 +61,206 @@ function* generateParserReference(
   const { names, types } = specification;
   const typeItem = types[typeKey];
   if (typeItem.id == null) {
-    yield itt`((value: unknown) => {
-      ${generateParserDefinition(specification, typeKey)}
-    })(${valueExpression})`;
+    yield itt`(${generateParserDefinition(specification, typeKey, valueExpression)})`;
   } else {
     const functionName = toCamel("parse", names[typeItem.id]);
     yield itt`${functionName}(${valueExpression}, configuration)`;
   }
 }
 
-function* generateParserDefinition(specification: models.Specification, typeKey: string) {
+function* generateParserDefinition(
+  specification: models.Specification,
+  typeKey: string,
+  valueExpression: string,
+) {
   const { names, types } = specification;
   const typeItem = types[typeKey];
 
   switch (typeItem.type) {
     case "unknown":
-      yield "return value;";
+      yield valueExpression;
       break;
 
     case "never":
-      yield "return undefined;";
+      yield "undefined";
       break;
 
     case "any":
-      yield "return value;";
+      yield valueExpression;
       break;
 
     case "null":
       yield `
-        if(value == null) {
-          return null;
-        }
-        
-        if(Array.isArray(value)) {
-          switch(value.length) {
-            case 0:
-              return null;
-            case 1:
-              [value] = value              
-              break;
-            default:
-              return undefined;
+        ((value: unknown) => {
+          if(value == null) {
+            return null;
           }
-        }
-        
-        switch(typeof value) {
-          case "string":
-            if(value.trim() === "") {
-              return null;
+          
+          if(Array.isArray(value)) {
+            switch(value.length) {
+              case 0:
+                return null;
+              case 1:
+                [value] = value              
+                break;
+              default:
+                return undefined;
             }
-            break;
-          case "number":
-            return Boolean(value);
-          case "boolean":
-            return value;
-        }
-        
-        return undefined;
+          }
+          
+          switch(typeof value) {
+            case "string":
+              if(value.trim() === "") {
+                return null;
+              }
+              break;
+            case "number":
+              return Boolean(value);
+            case "boolean":
+              return value;
+          }
+          
+          return undefined;
+        })(${valueExpression})
       `;
       break;
 
     case "boolean":
       yield `
-        if(value == null) {
-          return false;
-        }
-
-        if(Array.isArray(value)) {
-          switch(value.length) {
-            case 0:
-              return false;
-            case 1:
-              [value] = value              
-              break;
-            default:
-              return undefined;
+        ((value: unknown) => {
+          if(value == null) {
+            return false;
           }
-        }
 
-        switch(typeof value) {
-          case "string":
-            value = value.trim();
-            for(const trueStringValue of configuration.trueStringValues) {
-              if(value === trueStringValue) {
-                return true;
-              }
-            }
-            for(const falseStringValue of configuration.falseStringValues) {
-              if(value === falseStringValue) {
+          if(Array.isArray(value)) {
+            switch(value.length) {
+              case 0:
                 return false;
-              }
+              case 1:
+                [value] = value              
+                break;
+              default:
+                return undefined;
             }
-            return undefined;
-          case "number":
-            return Boolean(value);
-          case "boolean":
-            return value;
-        }
-        return undefined;
+          }
+
+          switch(typeof value) {
+            case "string":
+              value = value.trim();
+              for(const trueStringValue of configuration.trueStringValues) {
+                if(value === trueStringValue) {
+                  return true;
+                }
+              }
+              for(const falseStringValue of configuration.falseStringValues) {
+                if(value === falseStringValue) {
+                  return false;
+                }
+              }
+              return undefined;
+            case "number":
+              return Boolean(value);
+            case "boolean":
+              return value;
+          }
+          return undefined;
+          })(${valueExpression})
       `;
       break;
 
     case "integer":
       yield `
-        if(Array.isArray(value)) {
-          switch(value.length) {
-            case 1:
-              [value] = value              
-              break;
-            default:
-              return undefined;
+        ((value: unknown) => {
+          if(Array.isArray(value)) {
+            switch(value.length) {
+              case 1:
+                [value] = value              
+                break;
+              default:
+                return undefined;
+            }
           }
-        }
 
-        switch(typeof value) {
-          case "string":
-            return Number(value);
-          case "number":
-            return value;
-          case "boolean":
-            return value ? 1 : 0;
-        }
-        return undefined;
+          switch(typeof value) {
+            case "string":
+              return Number(value);
+            case "number":
+              return value;
+            case "boolean":
+              return value ? 1 : 0;
+          }
+          return undefined;
+        })(${valueExpression})
       `;
       break;
 
     case "number":
       yield `
-        if(Array.isArray(value)) {
-          switch(value.length) {
-            case 1:
-              [value] = value              
-              break;
-            default:
-              return undefined;
+        ((value: unknown) => {
+          if(Array.isArray(value)) {
+            switch(value.length) {
+              case 1:
+                [value] = value              
+                break;
+              default:
+                return undefined;
+            }
           }
-        }
-  
-        switch(typeof value) {
-          case "string":
-            return Number(value);
-          case "number":
-            return value;
-          case "boolean":
-            return value ? 1 : 0;
-        }
-        return undefined;
+    
+          switch(typeof value) {
+            case "string":
+              return Number(value);
+            case "number":
+              return value;
+            case "boolean":
+              return value ? 1 : 0;
+          }
+          return undefined;
+        })(${valueExpression})
       `;
       break;
 
     case "string":
       yield `
-        if(Array.isArray(value)) {
-          switch(value.length) {
-            case 1:
-              [value] = value              
-              break;
+        ((value: unknown) => {
+          if(Array.isArray(value)) {
+            switch(value.length) {
+              case 1:
+                [value] = value              
+                break;
+              default:
+                return undefined;
+            }
+          }
+
+          switch(typeof value) {
+            case "string":
+              return value;
+            case "number":
+            case "boolean":
+              return String(value);
             default:
               return undefined;
           }
-        }
-
-        switch(typeof value) {
-          case "string":
-            return value;
-          case "number":
-          case "boolean":
-            return String(value);
-          default:
-            return undefined;
-        }
+        })(${valueExpression})
       `;
       break;
 
     case "tuple": {
       yield itt`
-        return Array.isArray(value) ?
+        Array.isArray(${valueExpression}) ?
           [
             ${typeItem.elements.map(
               (element, index) => itt`
                 ${generateParserReference(
                   specification,
                   element,
-                  `value[${JSON.stringify(index)}]`,
+                  `${valueExpression}[${JSON.stringify(index)}]`,
                 )},
               `,
             )}
           ] :
-          undefined;
+          undefined
       `;
       break;
     }
@@ -256,28 +268,32 @@ function* generateParserDefinition(specification: models.Specification, typeKey:
     case "array": {
       const { element } = typeItem;
       yield itt`
-        return Array.isArray(value) ?
-          value.map(value => ${generateParserReference(specification, element, "value")}) :
-          undefined;
-        `;
+        Array.isArray(${valueExpression}) ?
+        ${valueExpression}.map(value => ${generateParserReference(
+          specification,
+          element,
+          "value",
+        )}) :
+          undefined
+      `;
       break;
     }
 
     case "object": {
       yield itt`
-        return (typeof value === "object" && value !== null && !Array.isArray(value)) ?
+        (typeof ${valueExpression} === "object" && ${valueExpression} !== null && !Array.isArray(${valueExpression})) ?
           {
             ${Object.entries(typeItem.properties).map(
               ([name, { required, element }]) => itt`
                 ${JSON.stringify(name)}: ${generateParserReference(
                   specification,
                   element,
-                  `value[${JSON.stringify(name)} as keyof typeof value]`,
+                  `${valueExpression}[${JSON.stringify(name)} as keyof typeof ${valueExpression}]`,
                 )},
               `,
             )}
           } :
-          undefined;
+          undefined
       `;
       break;
     }
@@ -285,30 +301,30 @@ function* generateParserDefinition(specification: models.Specification, typeKey:
     case "map": {
       const { name, element } = typeItem;
       yield itt`
-        return (typeof value === "object" && value !== null && !Array.isArray(value)) ?
+        (typeof ${valueExpression} === "object" && ${valueExpression} !== null && !Array.isArray(${valueExpression})) ?
           Object.fromEntries(
-            Object.entries(value).map(([name, value]) => [
+            Object.entries(${valueExpression}).map(([name, value]) => [
               ${generateParserReference(specification, name, "name")},
               ${generateParserReference(specification, element, "value")},
             ])
           ) :
-          undefined;
+          undefined
       `;
       break;
     }
 
     case "union": {
-      yield itt`return ${joinIterable(
+      yield joinIterable(
         typeItem.elements.map(
           (element) => itt`${generateParserReference(specification, element, "value")}`,
         ),
         " ?? ",
-      )};`;
+      );
       break;
     }
 
     case "alias": {
-      yield itt`return ${generateParserReference(specification, typeItem.target, "value")};`;
+      yield generateParserReference(specification, typeItem.target, "value");
       break;
     }
   }
