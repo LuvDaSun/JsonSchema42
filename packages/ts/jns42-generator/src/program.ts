3f0e3bb1cf3b3a5f5835924cb146bb85b89e06e3
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-24 11:42:26 +0100

    use global process

--- packages/ts/jns42-generator/src/program.ts
+++ packages/ts/jns42-generator/src/program.ts
@@ -3,14 +3,14 @@ import yargs from "yargs/yargs";
 import * as programs from "./programs/index.js";
 import { packageInfo } from "./utils/index.js";
 
-await main(process.argv, process.env);
+await main();
 
-async function main(argv: string[], env: Record<string, string | undefined>) {
+async function main() {
   const program = yargs(hideBin(process.argv));
 
   programs.configurePackageProgram(program);
   programs.configureIntermediateProgram(program);
-  if (env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "development") {
     // only enabled when debugging
     programs.configureTestProgram(program);
   }
