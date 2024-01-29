325c7a62686c0ee79c912e99a40340ae11b7b44d
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-29 18:39:39 +0100

    export

--- packages/ts/jns42-generator/src/main.ts
+++ packages/ts/jns42-generator/src/main.ts
@@ -3,6 +3,9 @@ export * from "./generators/index.js";
 export * from "./models/index.js";
 export * from "./utils/index.js";
 
+export * as oasV30 from "./documents/oas-v3-0/index.js";
 export * as schemaDraft04 from "./documents/schema-draft-04/index.js";
 export * as schemaDraft202012 from "./documents/schema-draft-2020-12/index.js";
 export * as schemaIntermediate from "./documents/schema-intermediate/index.js";
+export * as schemaOasV31 from "./documents/schema-oas-v3-1/index.js";
+export * as swaggerV2 from "./documents/swagger-v2/index.js";
