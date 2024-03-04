57203d2832c35a88c2e6a58a26733910bc41bb4b
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-04 12:25:38 +0100

    flatten

--- packages/cargo/jns42-generator/src/schema_transforms/flatten.rs
+++ packages/cargo/jns42-generator/src/schema_transforms/flatten.rs
@@ -1,4 +1,5 @@
 use crate::models::{arena::Arena, schema::SchemaNode};
+use std::iter::once;
 
 pub fn flatten_transform(arena: &mut Arena<SchemaNode>, key: usize) {
   let item = arena.get_item(key);
@@ -10,18 +11,50 @@ pub fn flatten_transform(arena: &mut Arena<SchemaNode>, key: usize) {
       sub_keys
         .iter()
         .copied()
-        .map(|sub_key| (sub_key, arena.get_item(*sub_key)))
-        .flat_map(|(sub_key, sub_item)| Some(*sub_key))
+        .map(|sub_key| (sub_key, arena.get_item(sub_key)))
+        .flat_map(|(sub_key, sub_item)| {
+          if let Some(sub_sub_keys) = &sub_item.all_of {
+            sub_sub_keys.clone()
+          } else {
+            once(sub_key).collect()
+          }
+        })
         .collect(),
     );
   }
 
   if let Some(sub_keys) = &item.any_of {
-    //
+    item_new.any_of = Some(
+      sub_keys
+        .iter()
+        .copied()
+        .map(|sub_key| (sub_key, arena.get_item(sub_key)))
+        .flat_map(|(sub_key, sub_item)| {
+          if let Some(sub_sub_keys) = &sub_item.any_of {
+            sub_sub_keys.clone()
+          } else {
+            once(sub_key).collect()
+          }
+        })
+        .collect(),
+    );
   }
 
   if let Some(sub_keys) = &item.one_of {
-    //
+    item_new.one_of = Some(
+      sub_keys
+        .iter()
+        .copied()
+        .map(|sub_key| (sub_key, arena.get_item(sub_key)))
+        .flat_map(|(sub_key, sub_item)| {
+          if let Some(sub_sub_keys) = &sub_item.one_of {
+            sub_sub_keys.clone()
+          } else {
+            once(sub_key).collect()
+          }
+        })
+        .collect(),
+    );
   }
 
   if item != &item_new {
@@ -39,19 +72,26 @@ mod tests {
     let mut arena = Arena::new();
 
     arena.add_item(SchemaNode {
-      all_of: Some([100, 200].into()),
+      all_of: Some([4, 5].into()),
       ..Default::default()
     });
 
     arena.add_item(SchemaNode {
-      any_of: Some([300, 400].into()),
+      all_of: Some([6, 7].into()),
       ..Default::default()
     });
 
     arena.add_item(SchemaNode {
-      one_of: Some([0, 1, 500, 600].into()),
+      all_of: Some([1, 2].into()),
       ..Default::default()
     });
+
+    arena.add_item(Default::default());
+    arena.add_item(Default::default());
+    arena.add_item(Default::default());
+    arena.add_item(Default::default());
+    arena.add_item(Default::default());
+    arena.add_item(Default::default());
 
     while arena.apply_transform(flatten_transform) > 0 {
       //
@@ -60,17 +100,23 @@ mod tests {
     let actual: Vec<_> = arena.iter().cloned().collect();
     let expected: Vec<_> = [
       SchemaNode {
-        all_of: Some([100, 200].into()),
+        all_of: Some([4, 5].into()),
         ..Default::default()
       },
       SchemaNode {
-        all_of: Some([300, 400].into()),
+        all_of: Some([6, 7].into()),
         ..Default::default()
       },
       SchemaNode {
-        all_of: Some([100, 200, 300, 400, 500, 600].into()),
+        all_of: Some([4, 5, 6, 7, 8, 9].into()),
         ..Default::default()
       },
+      Default::default(),
+      Default::default(),
+      Default::default(),
+      Default::default(),
+      Default::default(),
+      Default::default(),
     ]
     .into();
 
