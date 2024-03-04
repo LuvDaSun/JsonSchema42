f574b71497c2e958c7459fdc12860f94d0fce1c2
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-04 13:10:15 +0100

    wip

--- packages/cargo/jns42-generator/src/models/specification.rs
+++ packages/cargo/jns42-generator/src/models/specification.rs
@@ -243,27 +243,27 @@ impl Specification {
     // then optimize the schemas
 
     {
+      while arena.apply_transform(transformer) > 0 {
+        //
+      }
+
       fn transformer(arena: &mut Arena<SchemaNode>, key: usize) {
         schema_transforms::single_type::single_type_transform(arena, key);
         schema_transforms::explode::explode_transform(arena, key);
         schema_transforms::reference::reference_transform(arena, key);
         schema_transforms::flatten::flatten_transform(arena, key);
       }
-
-      while arena.apply_transform(transformer) > 0 {
-        //
-      }
     }
 
     // then set schema primary field
 
     {
-      fn transformer(arena: &mut Arena<SchemaNode>, key: usize) {
-        schema_transforms::primary::primary_transform(arena, key);
-      }
-
       while arena.apply_transform(transformer) > 0 {
         //
+      }
+
+      fn transformer(arena: &mut Arena<SchemaNode>, key: usize) {
+        schema_transforms::primary::primary_transform(arena, key);
       }
     }
 
