0658e1a95731d242b503d1abf43cac553ad2ac36
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-08 14:06:29 +0100

    format

--- packages/cargo/jns42-generator/src/utils/read_json_node.rs
+++ packages/cargo/jns42-generator/src/utils/read_json_node.rs
@@ -2,22 +2,22 @@ use serde_json::Value;
 use std::iter::once;
 
 pub fn read_json_node(prefix: String, node: Value) -> Vec<(String, Value)> {
-    match &node {
-        Value::Array(array_value) => once((prefix.clone(), node.clone()))
-            .chain(
-                array_value
-                    .iter()
-                    .enumerate()
-                    .flat_map(|(index, element_value)| {
-                        read_json_node(format!("{}/{}", prefix, index), element_value.clone())
-                    }),
-            )
-            .collect(),
-        Value::Object(object_value) => once((prefix.clone(), node.clone()))
-            .chain(object_value.iter().flat_map(|(name, element_value)| {
-                read_json_node(format!("{}/{}", prefix, name), element_value.clone())
-            }))
-            .collect(),
-        _ => once((prefix, node)).collect(),
-    }
+  match &node {
+    Value::Array(array_value) => once((prefix.clone(), node.clone()))
+      .chain(
+        array_value
+          .iter()
+          .enumerate()
+          .flat_map(|(index, element_value)| {
+            read_json_node(format!("{}/{}", prefix, index), element_value.clone())
+          }),
+      )
+      .collect(),
+    Value::Object(object_value) => once((prefix.clone(), node.clone()))
+      .chain(object_value.iter().flat_map(|(name, element_value)| {
+        read_json_node(format!("{}/{}", prefix, name), element_value.clone())
+      }))
+      .collect(),
+    _ => once((prefix, node)).collect(),
+  }
 }
--- packages/cargo/jns42-generator/src/utils/read_stream.rs
+++ packages/cargo/jns42-generator/src/utils/read_stream.rs
@@ -1,7 +1,7 @@
 use futures_util::{FutureExt, Stream};
 use std::{
-    pin::Pin,
-    task::{Context, Poll},
+  pin::Pin,
+  task::{Context, Poll},
 };
 use tokio::io::{AsyncRead, AsyncReadExt};
 
@@ -10,76 +10,76 @@ const BUFFER_SIZE: usize = 16;
 
 pub struct ReadStream<R>
 where
-    R: AsyncRead,
+  R: AsyncRead,
 {
-    reader: R,
-    at_end: bool,
+  reader: R,
+  at_end: bool,
 }
 
 impl<R> ReadStream<R>
 where
-    R: AsyncRead + Unpin,
+  R: AsyncRead + Unpin,
 {
-    pub fn new(reader: R) -> Self {
-        Self {
-            reader,
-            at_end: false,
-        }
+  pub fn new(reader: R) -> Self {
+    Self {
+      reader,
+      at_end: false,
     }
+  }
 }
 
 impl<R> Stream for ReadStream<R>
 where
-    Self: Unpin,
-    R: AsyncRead + Unpin,
+  Self: Unpin,
+  R: AsyncRead + Unpin,
 {
-    type Item = std::io::Result<Vec<u8>>;
+  type Item = std::io::Result<Vec<u8>>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let self_mut = self.get_mut();
+  fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    let self_mut = self.get_mut();
 
-        let reader = &mut self_mut.reader;
-        let mut chunk = [0; BUFFER_SIZE];
+    let reader = &mut self_mut.reader;
+    let mut chunk = [0; BUFFER_SIZE];
 
-        // this is where we store data for this poll event
-        let mut buffer = Vec::new();
+    // this is where we store data for this poll event
+    let mut buffer = Vec::new();
 
-        // loop only of we are not at the end
-        while !self_mut.at_end {
-            let mut read_future = Box::pin(reader.read(&mut chunk));
-            match read_future.poll_unpin(cx) {
-                // no data read, we assume that the reader is at it's end
-                Poll::Ready(Ok(0)) => {
-                    self_mut.at_end = true;
-                    break;
-                }
-                // read some data
-                Poll::Ready(Ok(count)) => {
-                    buffer.extend_from_slice(&chunk[..count]);
-                }
-                // something is wrong!
-                Poll::Ready(Err(error)) => {
-                    return Poll::Ready(Some(Err(error)));
-                }
-                // nothing to read, come back later!
-                Poll::Pending => {
-                    if buffer.is_empty() {
-                        // if there is nothing in the result buffer then come back later
-                        return Poll::Pending;
-                    } else {
-                        // if there is something in the buffer then exit the loop and let the code that follows emit the buffer
-                        break;
-                    }
-                }
-            }
+    // loop only of we are not at the end
+    while !self_mut.at_end {
+      let mut read_future = Box::pin(reader.read(&mut chunk));
+      match read_future.poll_unpin(cx) {
+        // no data read, we assume that the reader is at it's end
+        Poll::Ready(Ok(0)) => {
+          self_mut.at_end = true;
+          break;
         }
-
-        if self_mut.at_end && buffer.is_empty() {
-            // we are done!
-            Poll::Ready(None)
-        } else {
-            // emit whatever we just read
-            Poll::Ready(Some(Ok(buffer)))
+        // read some data
+        Poll::Ready(Ok(count)) => {
+          buffer.extend_from_slice(&chunk[..count]);
         }
+        // something is wrong!
+        Poll::Ready(Err(error)) => {
+          return Poll::Ready(Some(Err(error)));
+        }
+        // nothing to read, come back later!
+        Poll::Pending => {
+          if buffer.is_empty() {
+            // if there is nothing in the result buffer then come back later
+            return Poll::Pending;
+          } else {
+            // if there is something in the buffer then exit the loop and let the code that follows emit the buffer
+            break;
+          }
+        }
+      }
     }
+
+    if self_mut.at_end && buffer.is_empty() {
+      // we are done!
+      Poll::Ready(None)
+    } else {
+      // emit whatever we just read
+      Poll::Ready(Some(Ok(buffer)))
+    }
+  }
 }
