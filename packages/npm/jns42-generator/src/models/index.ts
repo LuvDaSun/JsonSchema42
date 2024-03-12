2b484ed7ac244daab3134b44448fd18b7bb123eb
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-12 11:43:01 +0100

    no circular deps

--- packages/npm/jns42-generator/src/models/index.ts
+++ packages/npm/jns42-generator/src/models/index.ts
@@ -1,5 +1,4 @@
 export * from "./arena.js";
 export * from "./item.js";
-export * from "./merge.js";
 export * from "./selectors.js";
 export * from "./specification.js";
--- packages/npm/jns42-generator/src/schema-transforms/resolve-all-of.ts
+++ packages/npm/jns42-generator/src/schema-transforms/resolve-all-of.ts
@@ -1,4 +1,4 @@
-import { SchemaItem, SchemaTransform, intersectionMergeTypes } from "../models/index.js";
+import { SchemaItem, SchemaTransform, SchemaType } from "../models/index.js";
 import {
   booleanMergeAnd,
   booleanMergeOr,
@@ -180,3 +180,46 @@ export const resolveAllOf: SchemaTransform = (arena, key) => {
     return keyNew;
   }
 };
+
+function intersectionMergeTypes(
+  types: SchemaType[] | undefined,
+  otherTypes: SchemaType[] | undefined,
+): SchemaType[] | undefined {
+  if (types === otherTypes) {
+    return types;
+  }
+
+  if (types == null) {
+    return otherTypes;
+  }
+  if (otherTypes == null) {
+    return types;
+  }
+
+  if (types.length === 0) {
+    return otherTypes;
+  }
+  if (otherTypes.length === 0) {
+    return types;
+  }
+
+  if (types.length > 1 || otherTypes.length > 1) {
+    throw new TypeError("can only merge single types");
+  }
+
+  const [type] = types;
+  const [otherType] = otherTypes;
+
+  if (type === "any") {
+    return otherTypes;
+  }
+  if (otherType === "any") {
+    return types;
+  }
+
+  if (type === otherType) {
+    return types;
+  }
+
+  return ["never"];
+}
