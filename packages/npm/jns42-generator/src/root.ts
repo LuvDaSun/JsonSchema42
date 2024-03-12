ffa8daa03a0cc00b6092de0926c7522cd63272bc
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-12 10:47:21 +0100

    dirname

--- packages/npm/jns42-generator/src/root.ts
+++ packages/npm/jns42-generator/src/root.ts
@@ -3,6 +3,6 @@ import * as path from "path";
 export const projectRoot = makeProjectRoot();
 
 function makeProjectRoot() {
-  const dirname = import.meta.dirname ?? (() => __dirname)();
+  const dirname = import.meta.dirname ?? global["__dirname"];
   return path.resolve(dirname, "..");
 }
