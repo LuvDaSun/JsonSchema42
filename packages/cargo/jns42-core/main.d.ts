befdb658d419642d6969da681cf7248e7005f4c5
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-18 13:59:39 +0100

    custom d ts

---
+++ packages/cargo/jns42-core/main.d.ts
@@ -0,0 +1,5 @@
+export function toSnake(parts: string[]): string;
+export function toPascal(parts: string[]): string;
+export function toCamel(parts: string[]): string;
+
+export function product<T>(sets: T[][]): T[][];
