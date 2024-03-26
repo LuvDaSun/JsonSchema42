d515aea93fd602920ede8ed0c1002d8659b809e3
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-26 14:36:45 +0100

    wip

--- packages/npm/jns42-core/src/ffi.ts
+++ packages/npm/jns42-core/src/ffi.ts
@@ -3,15 +3,6 @@ import fs from "node:fs";
 import path from "path";
 import { projectRoot } from "./root.js";
 
-export function reverse(value: string): string {
-  using resultOut = Out.createNullReference();
-  using valuePascalString = PascalString.fromString(value);
-  wasmExports.reverse(valuePascalString.asPointer(), resultOut.asPointer());
-  using resultPascalString = PascalString.fromPointer(resultOut.getReference());
-  const result = resultPascalString.toString();
-  return result;
-}
-
 //#region wasm
 
 type Size = number;
