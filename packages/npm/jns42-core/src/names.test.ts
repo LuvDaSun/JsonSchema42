006a9b8ddb49ab049e4672ce55b043e3d5bd8413
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-26 15:28:26 +0100

    wip

--- packages/npm/jns42-core/src/names.test.ts
+++ packages/npm/jns42-core/src/names.test.ts
@@ -1,5 +1,6 @@
 import assert from "node:assert";
 import test from "node:test";
+import { wasmExports } from "./ffi.js";
 import { NamesBuilder } from "./names.js";
 
 test("names", () => {
@@ -29,5 +30,43 @@ test("names", () => {
     const actual = names.toSnakeCase(3);
     const expected = "id_dog";
     assert.equal(actual, expected);
+  }
+});
+
+test("names leak test", () => {
+  function runTest() {
+    using namesBuilder = NamesBuilder.new()
+      .add(1, "cat")
+      .add(1, "id")
+      .add(2, "cat")
+      .add(2, "name")
+      .add(3, "dog")
+      .add(3, "id");
+
+    using names = namesBuilder.build(5);
+
+    {
+      const actual = names.toSnakeCase(1);
+      const expected = "id_cat";
+      assert.equal(actual, expected);
+    }
+
+    {
+      const actual = names.toSnakeCase(2);
+      const expected = "name";
+      assert.equal(actual, expected);
+    }
+
+    {
+      const actual = names.toSnakeCase(3);
+      const expected = "id_dog";
+      assert.equal(actual, expected);
+    }
+  }
+  const byteLength = wasmExports.memory.buffer.byteLength;
+  for (let index = 0; index < 100000; index++) {
+    runTest();
+
+    assert.equal(wasmExports.memory.buffer.byteLength, byteLength);
   }
 });
