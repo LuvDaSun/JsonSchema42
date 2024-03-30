f6f3d90e900ba2cf23eaaebdb30db6bd85184a1e
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-30 11:09:24 +0100

    rename to title

--- packages/npm/jns42-core/src/ffi/schema-item.ts
+++ packages/npm/jns42-core/src/ffi/schema-item.ts
@@ -1,16 +1,16 @@
 import assert from "assert";
 import { NULL_POINTER, Pointer } from "../utils/ffi.js";
-import { PascalString } from "./pascal-string.js";
+import { Utf8String } from "./string.js";
 import { Structure } from "./structure.js";
 
 const SIZE = 2 * 4;
 
 const ID_OFFSET = 0 * 4;
-const NAME_OFFSET = 1 * 4;
+const TITLE_OFFSET = 1 * 4;
 
 export class SchemaItem extends Structure {
-  private id = PascalString.fromPointer(this.pointer + ID_OFFSET);
-  private name = PascalString.fromPointer(this.pointer + NAME_OFFSET);
+  private id = Utf8String.fromPointer(this.pointer + ID_OFFSET);
+  private title = Utf8String.fromPointer(this.pointer + TITLE_OFFSET);
 
   protected constructor(pointer: Pointer) {
     super(pointer, SIZE);
@@ -24,7 +24,7 @@ export class SchemaItem extends Structure {
 
   [Symbol.dispose]() {
     this.id[Symbol.dispose]();
-    this.name[Symbol.dispose]();
+    this.title[Symbol.dispose]();
 
     super[Symbol.dispose]();
   }
