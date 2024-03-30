88d662999b35c7fc2cfe925789a93023b80cf91f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-30 21:15:18 +0100

    wip

--- packages/npm/jns42-core/src/ffi/schema-item.ts
+++ packages/npm/jns42-core/src/ffi/schema-item.ts
@@ -3,14 +3,51 @@ import { NULL_POINTER, Pointer } from "../utils/ffi.js";
 import { SizedString } from "./sized-string.js";
 import { Structure } from "./structure.js";
 
-const SIZE = 2 * 4;
+const ID_OFFSET = 0;
+const TITLE_OFFSET = 4;
+const DEPRECATED_OFFSET = 8;
 
-const ID_OFFSET = 0 * 4;
-const TITLE_OFFSET = 1 * 4;
+const SIZE = 9;
 
 export class SchemaItem extends Structure {
-  private id = SizedString.fromPointer(this.pointer + ID_OFFSET);
-  private title = SizedString.fromPointer(this.pointer + TITLE_OFFSET);
+  public get id() {
+    const pointer = this.getInt32(this.pointer + ID_OFFSET);
+    if (pointer === NULL_POINTER) {
+      return undefined;
+    } else {
+      return SizedString.fromPointer(pointer);
+    }
+  }
+  public set id(value: SizedString | undefined) {
+    if (value == null) {
+      this.setInt32(this.pointer + ID_OFFSET, NULL_POINTER);
+    } else {
+      this.setInt32(this.pointer + ID_OFFSET, value.pointer);
+    }
+  }
+
+  public get title() {
+    const pointer = this.getInt32(this.pointer + TITLE_OFFSET);
+    if (pointer === NULL_POINTER) {
+      return undefined;
+    } else {
+      return SizedString.fromPointer(pointer);
+    }
+  }
+  public set title(value: SizedString | undefined) {
+    if (value == null) {
+      this.setInt32(this.pointer + TITLE_OFFSET, NULL_POINTER);
+    } else {
+      this.setInt32(this.pointer + TITLE_OFFSET, value.pointer);
+    }
+  }
+
+  public get deprecated() {
+    return Boolean(this.getInt8(DEPRECATED_OFFSET));
+  }
+  public set deprecated(value: boolean) {
+    this.setInt8(DEPRECATED_OFFSET, Number(value));
+  }
 
   protected constructor(pointer: Pointer) {
     super(pointer, SIZE);
@@ -22,10 +59,33 @@ export class SchemaItem extends Structure {
     return new SchemaItem(pointer);
   }
 
+  public static fromObject(object: SchemaItemObject) {
+    const instance = new SchemaItem(NULL_POINTER);
+
+    instance.id = object.id == null ? undefined : SizedString.fromString(object.id);
+    instance.title = object.title == null ? undefined : SizedString.fromString(object.title);
+
+    return instance;
+  }
+
+  public toObject() {
+    return {
+      id: this.id?.toString(),
+      title: this.title?.toString(),
+      deprecated: this.deprecated,
+    };
+  }
+
   [Symbol.dispose]() {
-    this.id[Symbol.dispose]();
-    this.title[Symbol.dispose]();
+    this.id?.[Symbol.dispose]();
+    this.title?.[Symbol.dispose]();
 
     super[Symbol.dispose]();
   }
+}
+
+export interface SchemaItemObject {
+  id?: string;
+  title?: string;
+  deprecated?: boolean;
 }
