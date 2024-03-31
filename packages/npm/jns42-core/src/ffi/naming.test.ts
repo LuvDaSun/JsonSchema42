72dee78400e9b84bfcce685b73b70311aa8ede77
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-31 13:13:41 +0200

    test

--- packages/npm/jns42-core/src/ffi/naming.test.ts
+++ packages/npm/jns42-core/src/ffi/naming.test.ts
@@ -70,3 +70,41 @@ test("names leak test", () => {
     assert.equal(mainFfi.exports.memory.buffer.byteLength, byteLength);
   }
 });
+
+test("names leak test 2", () => {
+  function runTest() {
+    const namesBuilder = NamesBuilder.new()
+      .add(1, "cat")
+      .add(1, "id")
+      .add(2, "cat")
+      .add(2, "name")
+      .add(3, "dog")
+      .add(3, "id");
+
+    const names = namesBuilder.build();
+
+    {
+      const actual = names.toSnakeCase(1);
+      const expected = "cat_id";
+      assert.equal(actual, expected);
+    }
+
+    {
+      const actual = names.toSnakeCase(2);
+      const expected = "name";
+      assert.equal(actual, expected);
+    }
+
+    {
+      const actual = names.toSnakeCase(3);
+      const expected = "dog_id";
+      assert.equal(actual, expected);
+    }
+  }
+  const byteLength = mainFfi.exports.memory.buffer.byteLength;
+  for (let index = 0; index < 100000; index++) {
+    runTest();
+
+    assert.equal(mainFfi.exports.memory.buffer.byteLength, byteLength);
+  }
+});
