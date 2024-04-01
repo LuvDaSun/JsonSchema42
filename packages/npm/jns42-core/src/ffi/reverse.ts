f31c60e9e02972572bbeaae20835d8c840b72e8d
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-01 15:51:26 +0200

    wip

--- packages/npm/jns42-core/src/ffi/reverse.ts
+++ packages/npm/jns42-core/src/ffi/reverse.ts
@@ -4,10 +4,10 @@ import { NULL_POINTER } from "../utils/index.js";
 import * as wrappers from "../wrappers/index.js";
 
 export function reverse(value: string): string {
-  using outputWrapper = wrappers.Box.allocate(NULL_POINTER);
+  using resultBoxWrapper = wrappers.Box.allocate(NULL_POINTER);
   using valueWrapper = wrappers.SizedString.allocate(value);
-  mainFfi.exports.reverse(valueWrapper.pointer, outputWrapper.pointer);
-  const resultPointer = mainFfi.memoryView.getInt32(outputWrapper.pointer, true);
+  mainFfi.exports.reverse(valueWrapper.pointer, resultBoxWrapper.pointer);
+  const resultPointer = mainFfi.memoryView.getInt32(resultBoxWrapper.pointer, true);
   using resultWrapper = new wrappers.SizedString(resultPointer);
   const result = resultWrapper.read();
   assert(result != null);
