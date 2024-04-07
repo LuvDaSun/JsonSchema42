9d9bd1efcef503f5db6831f2693e9b02a62c10a7
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-07 19:34:58 +0200

    cleanup

--- packages/cargo/jns42-core/src/executor.rs
+++ packages/cargo/jns42-core/src/executor.rs
@@ -3,7 +3,6 @@ use futures::{executor::LocalPool, Future};
 use std::cell::RefCell;
 
 pub fn spawn(task: impl Future<Output = ()> + 'static) {
-  // MANUAL_EXECUTOR.spawn_wake(task);
   EXECUTOR.with_borrow_mut(|pool| {
     let spawner = pool.spawner();
     spawner.spawn_local(task).unwrap();
@@ -12,12 +11,9 @@ pub fn spawn(task: impl Future<Output = ()> + 'static) {
 }
 
 pub fn wake() {
-  // MANUAL_EXECUTOR.wake_all();
   EXECUTOR.with_borrow_mut(|pool| pool.run_until_stalled());
 }
 
 thread_local! {
    static EXECUTOR: RefCell<LocalPool> = RefCell::new(LocalPool::new());
 }
-
-// static MANUAL_EXECUTOR: Lazy<Arc<ManualExecutor>> = Lazy::new(ManualExecutor::new);
