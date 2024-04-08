3ebfcfca42dbce1617299433eded86c89e59ef5c
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-08 22:50:01 +0200

    error in ffi

--- packages/npm/jns42-core/src/main-ffi.ts
+++ packages/npm/jns42-core/src/main-ffi.ts
@@ -21,19 +21,23 @@ export interface MainExports extends ExportsBase {
   c_string_drop(pointer: number): void;
   c_string_new(size: number): number;
 
-  reverse(value: number, result: number): void;
+  reverse(value: number, result: number, error_reference: number): void;
 
   names_builder_drop(names_builder: number): void;
   names_builder_new(): number;
   names_builder_add(names_builder: number, key: number, values: number): void;
-  names_builder_set_default_name(names_builder: number, value: number): void;
+  names_builder_set_default_name(
+    names_builder: number,
+    value: number,
+    error_reference: number,
+  ): void;
   names_builder_build(names_builder: number): number;
 
   names_drop(names: number): void;
-  names_to_camel_case(names: number, key: number): number;
-  names_to_pascal_case(names: number, key: number): number;
-  names_to_snake_case(names: number, key: number): number;
-  names_to_screaming_snake_case(names: number, key: number): number;
+  names_to_camel_case(names: number, key: number, error_reference: number): number;
+  names_to_pascal_case(names: number, key: number, error_reference: number): number;
+  names_to_snake_case(names: number, key: number, error_reference: number): number;
+  names_to_screaming_snake_case(names: number, key: number, error_reference: number): number;
 
   schema_item_drop(schema_item: number): void;
   schema_item_new(): number;
@@ -42,9 +46,14 @@ export interface MainExports extends ExportsBase {
   schema_arena_clone(schema_arena: number): number;
   schema_arena_new(): number;
   schema_arena_count(schema_arena: number): number;
-  schema_arena_add_item(schema_arena: number, item: number): number;
-  schema_arena_replace_item(schema_arena: number, key: number, item: number): number;
-  schema_arena_get_item(schema_arena: number, key: number): number;
+  schema_arena_add_item(schema_arena: number, item: number, error_reference: number): number;
+  schema_arena_replace_item(
+    schema_arena: number,
+    key: number,
+    item: number,
+    error_reference: number,
+  ): number;
+  schema_arena_get_item(schema_arena: number, key: number, error_reference: number): number;
   schema_arena_transform(schema_arena: number, vec: number): number;
 
   vec_usize_drop(vec_usize: number): void;
@@ -55,8 +64,8 @@ export interface MainExports extends ExportsBase {
   vec_string_drop(vec_usize: number): void;
   vec_string_new(capacity: number): number;
   vec_string_len(vec_usize: number): number;
-  vec_string_get(vec_usize: number, index: number): number;
-  vec_string_push(vec_usize: number, value: number): void;
+  vec_string_get(vec_usize: number, index: number, error_reference: number): number;
+  vec_string_push(vec_usize: number, value: number, error_reference: number): void;
 
   node_location_drop(node_location: number): void;
   node_location_clone(node_location: number): number;
@@ -74,13 +83,17 @@ export interface MainExports extends ExportsBase {
 
   document_context_drop(document_context: number): void;
   document_context_new(): number;
-  document_context_register_well_known_factories(document_context: number): void;
+  document_context_register_well_known_factories(
+    document_context: number,
+    error_reference: number,
+  ): void;
   document_context_load_from_location(
     document_context: number,
     retrieval_location: number,
     given_location: number,
     antecedent_location: number,
     default_schema_id: MetaSchemaId,
+    error_reference: number,
     callback: number,
   ): void;
   document_context_load_from_node(
@@ -90,14 +103,18 @@ export interface MainExports extends ExportsBase {
     antecedent_location: number,
     node: number,
     default_schema_id: MetaSchemaId,
+    error_reference: number,
     callback: number,
   ): void;
-  document_context_get_intermediate_document(document_context: number): number;
+  document_context_get_intermediate_document(
+    document_context: number,
+    error_reference: number,
+  ): number;
 
-  to_camel_case(value: number): number;
-  to_pascal_case(value: number): number;
-  to_snake_case(value: number): number;
-  to_screaming_snake_case(value: number): number;
+  to_camel_case(value: number, error_reference: number): number;
+  to_pascal_case(value: number, error_reference: number): number;
+  to_snake_case(value: number, error_reference: number): number;
+  to_screaming_snake_case(value: number, error_reference: number): number;
 }
 
 export type MainFfi = Ffi<MainExports, MainEnvironment>;
