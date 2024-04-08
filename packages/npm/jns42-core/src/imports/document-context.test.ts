039336dc60e83e15a213b381b6c093b2698f9a83
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-08 23:32:54 +0200

    wip

--- packages/npm/jns42-core/src/imports/document-context.test.ts
+++ packages/npm/jns42-core/src/imports/document-context.test.ts
@@ -2,19 +2,22 @@ import assert from "node:assert";
 import test from "node:test";
 import { DocumentContext } from "./document-context.js";
 
-test("load empty", async () => {
+test("load from node", async () => {
   using documentContext = DocumentContext.new();
   documentContext.registerWellKnownFactories();
 
   await documentContext.loadFromNode(
-    "schema.json",
-    "schema.json",
+    "string-or-boolean.json",
+    "string-or-boolean.json",
     undefined,
-    {},
+    {
+      $schema: "http://json-schema.org/draft-04/schema#",
+      type: ["string", "boolean"],
+    },
     "https://json-schema.org/draft/2020-12/schema",
   );
 
   const intermediateDocument = documentContext.getIntermediateDocument();
 
-  assert(intermediateDocument.schemas["schema.json#"] != null);
+  assert(intermediateDocument.schemas["string-or-boolean.json#"] != null);
 });
