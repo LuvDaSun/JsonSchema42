d383841ddbf3327217e10c690e30ebc498ccf505
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-09 17:24:46 +0200

    comments

--- packages/cargo/jns42-core/src/executor.rs
+++ packages/cargo/jns42-core/src/executor.rs
@@ -8,10 +8,32 @@ thread_local! {
   static EXECUTOR: RefCell<LocalPool> = RefCell::new(LocalPool::new());
 }
 
+/// Wakes up the local executor and runs all pending tasks.
+///
+/// This function is used to ensure that all pending tasks are executed when needed.
+/// It borrows the mutable reference to the local pool and runs all tasks until they are stalled.
+///
 pub fn wake() {
   EXECUTOR.with_borrow_mut(|pool| pool.run_until_stalled());
 }
 
+/// Spawns a new task and runs it on the local executor.
+///
+/// After the task is completed, it calls the provided `callback` function using the `host_invoke_callback` function.
+///
+/// # Arguments
+///
+/// * `callback` - A unique identifier for the callback function to be invoked.
+/// * `task` - A future that represents the task to be executed.
+///
+/// # Returns
+///
+/// This function does not return any value.
+///
+/// # Panics
+///
+/// This function may panic if the task fails to spawn or if the `host_invoke_callback` function fails.
+///
 pub fn spawn_and_callback(callback: Key, task: impl Future<Output = ()> + 'static) {
   EXECUTOR.with_borrow_mut(|pool| {
     let spawner = pool.spawner();
