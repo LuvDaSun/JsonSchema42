916d41bbf57ebc497c611ab456ff7d534ead7cd7
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-22 13:51:06 +0200

    tests

---
+++ packages/npm/jns42-core/src/imports/node-location.test.ts
@@ -0,0 +1,108 @@
+import assert from "node:assert";
+import test from "node:test";
+import { NodeLocation } from "./node-location.js";
+
+test("node-location push", () => {
+  {
+    using location = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i");
+    const pointer = location.getPointer() ?? [];
+    using actual = location.setPointer([...pointer, ...["j", "k", "l"]]);
+    using expected = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i/j/k/l");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+});
+
+test("node-location join", () => {
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i").join(
+      NodeLocation.parse("http://x.y.z/"),
+    );
+    using expected = NodeLocation.parse("http://x.y.z/");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i").join(
+      NodeLocation.parse("c:\\x"),
+    );
+    using expected = NodeLocation.parse("c:/x");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i").join(
+      NodeLocation.parse("/x/y/z"),
+    );
+    using expected = NodeLocation.parse("http://a.b.c/x/y/z#");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i").join(
+      NodeLocation.parse("/x/y/z/"),
+    );
+    using expected = NodeLocation.parse("http://a.b.c/x/y/z/#");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("c:\\a\\d\\e\\f").join(NodeLocation.parse("/x/y/z/"));
+    using expected = NodeLocation.parse("c:/x/y/z/#");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f#/g/h/i").join(
+      NodeLocation.parse("x/y/z/"),
+    );
+    using expected = NodeLocation.parse("http://a.b.c/d/e/x/y/z/#");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("c:\\a\\d\\e\\f").join(NodeLocation.parse("x/y/z/"));
+    using expected = NodeLocation.parse("c:/a/d/e/x/y/z/#");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f/#/g/h/i").join(
+      NodeLocation.parse("x/y/z/"),
+    );
+    using expected = NodeLocation.parse("http://a.b.c/d/e/f/x/y/z/#");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f/#/g/h/i").join(
+      NodeLocation.parse("#/x/y/z/"),
+    );
+    using expected = NodeLocation.parse("http://a.b.c/d/e/f/#/x/y/z/");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e/f/#/g/h/i").join(
+      NodeLocation.parse("?x=y"),
+    );
+    using expected = NodeLocation.parse("http://a.b.c/d/e/f/?x=y");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+  {
+    using actual = NodeLocation.parse("http://a.b.c/d/e?f#/g/h/i").join(NodeLocation.parse("?x=y"));
+    using expected = NodeLocation.parse("http://a.b.c/d/e?x=y");
+
+    assert.equal(actual.toString(), expected.toString());
+  }
+});
