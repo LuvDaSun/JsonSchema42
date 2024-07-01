4fab894a46660902f2922225b87bb469c31c2ed5
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-07-01 13:52:16 +0200

    setting up release strategy

---
+++ packages/npm/jns42-generator/scripts/postversion.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import cp from "child_process";
+
+const options = {
+  shell: true,
+  stdio: "inherit",
+  env: process.env,
+};
+
+const tag = `npm:${process.env.npm_package_name}@${process.env.npm_new_version}`;
+
+cp.execFileSync("git", ["add", process.env.npm_package_json], options);
+cp.execFileSync("git", ["commit", "--message", tag], options);
+cp.execFileSync("git", ["tag", tag], options);
