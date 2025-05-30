ce4111cd0767836d8e6a882a7a193f9522d1508b
Elmer Bulthuis <elmerbulthuis@gmail.com>
2025-05-30 13:06:37 +0700

    move plain to lib

--- packages/npm/jns42-generator/src/generators/types-ts.ts
+++ packages/npm/jns42-generator/src/generators/types-ts.ts
@@ -27,20 +27,6 @@ export function* generateTypesTsCode(specification: models.Specification) {
           readonly [_tags]: { [K in N]: void };
         }
       : T;
-      
-    export type Plain<T> = T extends undefined
-      ? boolean
-      : T extends number
-      ? number
-      : T extends bigint
-      ? bigint
-      : T extends string
-      ? string
-      : T extends symbol
-      ? symbol
-      : T extends object
-      ? { [K in Exclude<keyof T, symbol>]: Plain<T[K]> }
-      : T;
   `;
 
   for (let itemKey = 0; itemKey < typesArena.count(); itemKey++) {
--- packages/npm/jns42-lib/src/main.ts
+++ packages/npm/jns42-lib/src/main.ts
@@ -1,1 +1,2 @@
+export * as utilities from "./utilities.js";
 export * as validation from "./validation.js";
