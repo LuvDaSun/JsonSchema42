352b718f5c87102ab5cd22256817a86642e513df
Elmer Bulthuis <elmerbulthuis@gmail.com>
2025-05-31 15:28:27 +0700

    name

--- packages/npm/jns42-core/src/main.test.js
+++ packages/npm/jns42-core/src/main.test.js
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import test from "node:test";
 import core from "./main.js";
 
-test("ok", () => {
+test("banner", () => {
   const actual = core.utilities.banner("#", "0.0.0");
 
   assert(actual.startsWith("#"));
