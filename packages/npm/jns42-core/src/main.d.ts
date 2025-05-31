b1c8d8ca9e40fa5a8df1197dd71b28afc89cb287
Elmer Bulthuis <elmerbulthuis@gmail.com>
2025-06-01 02:37:41 +0700

    wip

--- packages/npm/jns42-core/src/main.d.ts
+++ packages/npm/jns42-core/src/main.d.ts
@@ -1,7 +1,5 @@
-import type { Root } from "../dist/jns42_core.component.js";
-
-export const documents: Root["documents"];
-export const models: Root["models"];
-export const naming: Root["naming"];
-export const schemaTransforms: Root["schemaTransforms"];
-export const utilities: Root["utilities"];
+export * as documents from "../dist/interfaces/jns42-core-documents.js"; // jns42:core/documents
+export * as models from "../dist/interfaces/jns42-core-models.js"; // jns42:core/models
+export * as naming from "../dist/interfaces/jns42-core-naming.js"; // jns42:core/naming
+export * as schemaTransforms from "../dist/interfaces/jns42-core-schema-transforms.js"; // jns42:core/schema-transforms
+export * as utilities from "../dist/interfaces/jns42-core-utilities.js"; // jns42:core/utilities
