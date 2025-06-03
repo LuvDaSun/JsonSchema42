217b68cb229b6c0d5f6839c1138d103d99456eec
Elmer Bulthuis <elmerbulthuis@gmail.com>
2025-06-04 01:05:02 +0700

    wip

--- packages/cargo/jns42-generator/src/lib.rs
+++ packages/cargo/jns42-generator/src/lib.rs
@@ -4,9 +4,8 @@ pub mod programs;
 
 #[cfg(test)]
 mod tests {
-
   jns42_macros::test_specifications!(
     "fixtures/specifications",
-    "packages/cargo/.generated/specifications"
+    "packages/cargo/jns42-generator/.generated/specifications"
   );
 }
