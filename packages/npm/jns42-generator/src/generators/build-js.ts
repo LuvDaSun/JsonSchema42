816b4982575ba28bb2a5177041b557a3968bf792
Elmer Bulthuis <108056+elmerbulthuis@users.noreply.github.com>
2025-06-09 11:54:43 +0700

    Tests as unit tests (#139)
    Co-authored-by: Elmer Bulthuis <elmerbulthuis@gmail.com>

--- packages/npm/jns42-generator/src/generators/build-js.ts
+++ packages/npm/jns42-generator/src/generators/build-js.ts
@@ -3,49 +3,16 @@ import { packageInfo } from "../utilities.js";
 import { itt } from "../utilities/iterable-text-template.js";
 
 export function* generateBuildJsCode() {
-  yield itt`
-    #!/usr/bin/env node
-  `;
-
   yield core.banner("//", `v${packageInfo.version}`);
 
   yield itt`
-    import cp from "child_process";
-    import path from "path";
+    import cp from "node:child_process";
   `;
 
   yield itt`
     const options = { shell: true, stdio: "inherit" };
 
-    cp.execFileSync("tsc", [], options);
-    
-    cp.execFileSync(
-      "rollup",
-      [
-        "--input",
-        path.resolve("transpiled", "main.js"),
-        "--file",
-        path.resolve("bundled", "main.js"),
-        "--sourcemap",
-        "--format",
-        "module",
-      ],
-      options,
-    );
-    
-    cp.execFileSync(
-      "rollup",
-      [
-        "--input",
-        path.resolve("transpiled", "main.js"),
-        "--file",
-        path.resolve("bundled", "main.cjs"),
-        "--sourcemap",
-        "--format",
-        "commonjs",
-      ],
-      options,
-    );
-    
+    cp.execFileSync("tsc", ["--build"], options);
+    cp.execFileSync("rollup", ["--config"], options);
   `;
 }
---
+++ packages/npm/jns42-lib/src/ok.test.ts
@@ -0,0 +1,6 @@
+import assert from "node:assert";
+import test from "node:test";
+
+await test("ok", () => {
+  assert(true);
+});
